61626cbd09785a5f057d4204e2608cada7299f64
ShawonAshraf <shawon13@live.com>
2021-02-17 22:49:17 +0100

    index page spider

---
+++ karwanbazaar/spiders/index.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class IndexSpider(scrapy.Spider):
+    name = "index"
+
+    def start_requests(self):
+        urls = [
+            "https://motikontho.wordpress.com/"
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # decode bytes to utf8 strings
+        decoded = response.body.decode("utf-8")
+
+        # write to file
+        with open("./output/index.html", "w") as f:
+            f.write(decoded)
+
+        # log
+        self.log("Saved file -> index.html")
