aeefeeee19309ee8b50a51df0260b4cbbd4f09ec
ShawonAshraf <shawon13@live.com>
2021-02-18 00:26:06 +0100

    change file format to txt

--- karwanbazaar/spiders/archives.py
+++ karwanbazaar/spiders/archives.py
@@ -6,7 +6,6 @@ Writes archive list urls to a json file
 
 import scrapy
 import os
-import json
 
 
 class ArchivesSpider(scrapy.Spider):
@@ -27,14 +26,10 @@ class ArchivesSpider(scrapy.Spider):
         # css query to get the urls
         archives_root_urls = selectors.css("ul li a::attr(href)").extract()
 
-        # create a dict for json
-        url_dict = dict()
-        for idx, url in enumerate(archives_root_urls):
-            url_dict[idx] = url
-
         # save urls to a file
-        with open(f"{os.getcwd()}/output/archive_root_urls.json", "w") as f:
-            json.dump(url_dict, f, indent=4)
+        with open(f"{os.getcwd()}/output/archive_root_urls.txt", "w") as f:
+            for url in archives_root_urls:
+                f.write(url + "\n")
 
         # log
-        self.log(f"Saved archives root urls -> {os.getcwd()}/output/archive_root_urls.json")
+        self.log(f"Saved archives root urls -> {os.getcwd()}/output/archive_root_urls.txt")
